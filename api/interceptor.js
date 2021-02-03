00a54b57effbe7456d25882c65ae39f9061ca546
Salvador Elvira <chavo45@hotmail.com>
2021-02-03 00:02:46 -0600

    Intercept the no return and remove jest

--- api/interceptor.js
+++ api/interceptor.js
@@ -1,19 +1,24 @@
 const REDIRECT_REQUEST_URLS = {
     '/files/batch': '/file-batch-api',
-}
+    '/api/files/batch': '/file-batch-api',
+};
+
 function batchInterceptor(instance) {
     instance.interceptors.request.use(
         request => {
-            console.log('Starting Request');
             return {
                 ...request,
                 withCredentials: true,
                 url: REDIRECT_REQUEST_URLS[request.url] ?? request.url,
             };
-            // Add your code herereturn request;
         }, 
         error => Promise.reject(error)
     );
+    instance.interceptors.response.use(({data, status}) => {
+        if(data.items.length === 0 || status !== 200) return Promise.reject('document-not-found');
+        return data.items;
+    },
+    (error) => Promise.reject(error));
 }
     
 export default batchInterceptor;
--- test.js
+++ test.js
@@ -1,16 +1,29 @@
 import apiClient from "./api/apiClient.js";
+import axios from "axios";
 // All requests should run at the same time and produce only one request
 // to the backend. All requests should return or reject.
 function runTest() {
     const batchUrl = "/file-batch-api";
 
     // Should return [{id:"fileid1"},{id:"fileid2"}]
-    apiClient.get(batchUrl, {params: {ids: ["fileid1","fileid2"]}}).then(response => console.log(response.data));
-    
+    const request1 = apiClient.get(batchUrl, {params: {ids: ["fileid1","fileid2"]}});
     // Should return [{id:"fileid2"}]
-    // apiClient.get(batchUrl, {params: {ids: ["fileid2"]}}).then(response => console.log(response.data));
+    const request2 = apiClient.get(batchUrl, {params: {ids: ["fileid2"]}});
     // Should reject as the fileid3 is missing from the response
-    // apiClient.get(batchUrl, {params: {ids: ["fileid3"]}}).then(response => console.log(response.data));
+    const request3 = apiClient.get(batchUrl, {params: {ids: ["fileid3"]}});
+
+    axios.all([request1, request2, request3]).then(axios.spread((...responses) => {
+        console.log(responses);
+        console.log(
+            Object.values(responses
+                .flat()
+                .reduce((acc, item) => {
+                    acc[item.id] = item;
+                    return acc;
+                }, {})));
+    })).catch(error => {
+        console.log(error);
+    });
 }
 
 runTest();
