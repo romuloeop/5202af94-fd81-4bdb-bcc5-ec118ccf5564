6e862d18d5e13b251a3a2f731e4d4f0ac22d2a1e
Salvador Elvira <chavo45@hotmail.com>
2021-02-05 02:03:00 -0600

    Implementing the batch with a timeout

--- api/interceptor.js
+++ api/interceptor.js
@@ -1,24 +1,52 @@
-const REDIRECT_REQUEST_URLS = {
-    '/files/batch': '/file-batch-api',
-    '/api/files/batch': '/file-batch-api',
-};
+const INTERCEPT_BATCH = '/api/files/batch​';
+const BATCH_API_PATH = '/file-batch-api';
+const WAITING_TIME_MS = 3000;
 
 function batchInterceptor(instance) {
+    let requestedIDs = [];
+    let batchPromise = false;
+    let timeout;
     instance.interceptors.request.use(
         request => {
+            if(request.url !== INTERCEPT_BATCH) return request;
+            request.params.ids.forEach(requestedId => {
+                if(!requestedIDs.includes(requestedId)) requestedIDs.push(requestedId);
+            });;
             return {
                 ...request,
-                withCredentials: true,
-                url: REDIRECT_REQUEST_URLS[request.url] ?? request.url,
+                adapter: (config) => {
+                    if(!batchPromise) batchPromise = new Promise((resolve) => {
+                        clearTimeout(timeout);
+                        timeout = setTimeout(() => {
+                            if(requestedIDs.length > 0) {
+                                instance.get(BATCH_API_PATH, {
+                                    params: {
+                                        ids: requestedIDs,
+                                    },
+                                }).then(response => {
+                                    clearTimeout(timeout);
+                                    requestedIDs = [];
+                                    batchPromise = false;
+                                    resolve(response);
+                                }).catch(error => {
+                                    reject(error);
+                                });
+                            }
+                        }, WAITING_TIME_MS);
+                    });
+                    return batchPromise.then(response => {
+                        const items = response.data?.items.filter(({id}) => config.params.ids.includes(id)) ?? [];
+                        if(items.length === 0) return Promise.reject('document-not-found');
+                        return { 
+                            ...response,
+                            data: { items }
+                        };
+                    });
+                },
             };
         }, 
         error => Promise.reject(error)
     );
-    instance.interceptors.response.use(({data, status}) => {
-        if(data.items.length === 0 || status !== 200) return Promise.reject('document-not-found');
-        return data.items;
-    },
-    (error) => Promise.reject(error));
 }
     
 export default batchInterceptor;
--- test.js
+++ test.js
@@ -3,27 +3,14 @@ import axios from "axios";
 // All requests should run at the same time and produce only one request
 // to the backend. All requests should return or reject.
 function runTest() {
-    const batchUrl = "/file-batch-api";
+    const batchUrl = "/api/files/batch​";
 
     // Should return [{id:"fileid1"},{id:"fileid2"}]
-    const request1 = apiClient.get(batchUrl, {params: {ids: ["fileid1","fileid2"]}});
+    apiClient.get(batchUrl, {params: {ids: ["fileid1","fileid2"]}}).then(({data}) => console.log(data));
     // Should return [{id:"fileid2"}]
-    const request2 = apiClient.get(batchUrl, {params: {ids: ["fileid2"]}});
+    apiClient.get(batchUrl, {params: {ids: ["fileid2"]}}).then(({data}) => console.log(data));
     // Should reject as the fileid3 is missing from the response
-    const request3 = apiClient.get(batchUrl, {params: {ids: ["fileid3"]}});
-
-    axios.all([request1, request2, request3]).then(axios.spread((...responses) => {
-        console.log(responses);
-        console.log(
-            Object.values(responses
-                .flat()
-                .reduce((acc, item) => {
-                    acc[item.id] = item;
-                    return acc;
-                }, {})));
-    })).catch(error => {
-        console.log(error);
-    });
+    apiClient.get(batchUrl, {params: {ids: ["fileid3"]}}).then(({data}) => console.log(data)).catch(error => console.log(error));
 }
 
 runTest();
